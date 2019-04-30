432a5649f22427e0bc6d30ba4a2c8ea95fb8e540
Fhulufhelo Mokhomi <mokhomifhulufhelo8@gmail.com>
2019-04-30 08:34:22 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,82 +1,57 @@
 <?php
 
-use app\Database;
+namespace App\Traits;
 
-class Publication 
+use App\Activity;
+use Illuminate\Support\Facades\Auth;
+
+
+trait Recordable
 {
-	public $db;
-	public $main_publication_business_day = [];
-	public $business_day_region
-	public $article;
-	
-	function __construct()
-	{
-		// initialize database
-		$this->db = new Database();
+    public static function boot()
+    {
+        parent::boot();
 
-		// Retrive Main publication of the business day
+        if (!Auth::check()) return;
 
-		$this->main_publication_business_day = $this->db->get();
-	}
+        foreach (static::getActivitiesToRecord() as $event) {
 
+            static::$event(function ($model) use ($event) {
+                $model->recordActivity($event);
+            });
+        }
 
-	//  This controller method handle the creation of new region publication business day 
+    }
 
-	public function save()
-	{
-		// create parent publication of Pages,Articles and Images
-		$this->business_day_region = $this->db->create(['Name' => 'Business Day KZN']);
+    protected static function getActivitiesToRecord()
+    {
+        return ['created', 'updated', 'deleted'];
+    }
 
 
-		// loop through the main publication business day and save duplicate to their region
+    protected function recordActivity($event)
+    {
 
-		foreach ($this->main_publication_business_day as $duplicate) {
-
-			/**
-			* as Article has one to many relationship ( two page can belong to one article )
-			* I created it first and save its object that will be later used when creating its corresponding child ( Image)
-			*  $this->db->create return created object
-			*/
-
-			$this->article = $this->db->create([
-				'Title' =>$duplicate->article->Title,
-				'Author' = $duplicate->pages->article->Author,
-				'Content' = $duplicate->pages->article->Content,
-				'Start_Page_Id' = $duplicate->pages->article->Start_Page_Id
-			]);
+        $this->actitvity()->create([
+            'user_id' => Auth::id(),
+            'type' => $this->getActivityType($event)
+        ]);
+    }
 
 
-			// if we have more than one pages per article, 
-			// let loop though each pages and create create each page before assign it to article
+    protected function getActivityType($event)
+    {
+        $type = strtolower((new \ReflectionClass($this))->getShortName());
+        return "{$event}_{$type}";
+    }
 
 
-			if (count(($duplicate->pages) > 1) {
-				$this->create_page_artice($duplicate);
-			} else {
-				$this->create_page_artice($duplicate);
-			}
-			
-			
-		}
-	}
-
-
-	public function create_page_artice($duplicate)
-	{
-		$page = $this->db->create([
-			'Publication_ID' =>$this->business_day_region->id,
-			'Jpeg_Location' = $duplicate->pages->Jpeg_Location,
-			'Page_Number' = $duplicate->pages->Page_Number
-		]);
-
-		$image = $this->db->create([
-			'Article_ID' =>$duplicate->$this->article->id,
-			'Page_ID' = $duplicate->$page->id,
-			'X_coords' = $duplicate->pages->image->X_coords,
-			'Y_coords' = $duplicate->pages->image->Y_coords,
-			'Width' = $duplicate->pages->image->Width,
-			'Height' = $duplicate->pages->image->Height
-		]);
-	}
-
+    public function actitvity()
+    {
+        return $this->morphMany('App\Activity', 'subject');
+    }
+    
 }
+
+
+
